bab748450ed93404022576ebc5c727cc77fef343
CyanFrost <thebestjavaprogrammer@gmail.com>
2015-07-21 12:53:35 -0500

    Create Main.java

---
+++ src/com/dayellecraft/cyanfrost/forhposej/main/Main.java
@@ -0,0 +1,20 @@
+package com.dayellecraft.cyanfrost.forhposej.main;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+
+	public void onEnable(){ //Called when the plugin is enabled.
+		Bukkit.getPluginManager().registerEvents(new Listeners(), this); //Register Listener in the PluginManager
+	}
+	
+	public void onDisable(){ //Called when the plugin is disabled.
+		
+	}
+	
+	public void onLoad(){ //Called when the plugin is loaded. Don't do any API Calls here.
+		
+	}
+	
+}
